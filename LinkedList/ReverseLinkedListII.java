3ef385a2d58fcf5ff2d7091106bab921d2a703bc
aayanksinghai <aayanksinghai02@gmail.com>
2025-08-20 11:49:54 +0530

    LL Ques

---
+++ LinkedList/ReverseLinkedListII.java
@@ -0,0 +1,32 @@
+/*
+ * Given the head of a singly linked list and two integers left and right where left <= right, 
+ * reverse the nodes of the list from position left to position right, and return the reversed list.
+ * 
+ * Link: https://leetcode.com/problems/reverse-linked-list-ii/description/?envType=problem-list-v2&envId=linked-list
+ */
+
+
+
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) 
+    { 
+        this.val = val; 
+    }
+    ListNode(int val, ListNode next) {
+         this.val = val; this.next = next; 
+    }
+}
+
+
+public class ReverseLinkedListII {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+    
+            
+    }
+}
