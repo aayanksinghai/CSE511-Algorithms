a5fc237de559b8daeaf637770f6c9be5b0b0f488
aayanksinghai <aayanksinghai02@gmail.com>
2025-08-21 01:28:35 +0530

    Validate Stack Sequences

---
+++ StacksQueues/ValidateStackSequences.java
@@ -0,0 +1,31 @@
+/*
+ * Given two integer arrays pushed and popped each with distinct values, return true 
+ * if this could have been the result of a sequence of push and pop operations 
+ * on an initially empty stack, or false otherwise.
+ *  
+ * Link: https://leetcode.com/problems/validate-stack-sequences/?envType=problem-list-v2
+ * 
+ * Author: AAYANK SINGHAI (MT2025001)
+ */
+
+package StacksQueues;
+
+import java.util.Stack;
+
+public class ValidateStackSequences {
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int j = 0;  //maintaining for poppedArray index
+
+        for(int i = 0; i < pushed.length; i++){
+            stack.push(pushed[i]);
+
+            while(!stack.isEmpty() && stack.peek() == popped[j]){
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty(); 
+    } 
+}
