2b7325e9a734a1cbdf825aaf67e337da65146bce
aayanksinghai <aayanksinghai02@gmail.com>
2025-08-20 15:19:27 +0530

    PartitionList

--- LinkedList/PartitionList.java
+++ LinkedList/PartitionList.java
@@ -17,6 +17,42 @@ class ListNode {
 }
 
 public class PartitionList {
+    public ListNode partition(ListNode head, int x) {
+        // Dummy head for the list of nodes with values less than x.
+        ListNode lessHead = new ListNode(0);
+        // Tail pointer for the "less than" list.
+        ListNode lessTail = lessHead;
+
+        // Dummy head for the list of nodes with values greater than or equal to x.
+        ListNode greaterHead = new ListNode(0);
+        // Tail pointer for the "greater than or equal to" list.
+        ListNode greaterTail = greaterHead;
+
+        ListNode current = head;
+
+        // Iterate through the original list once.
+        while (current != null) {
+            if (current.val < x) {
+                // Add the current node to the "less than" list.
+                lessTail.next = current;
+                lessTail = lessTail.next;
+            } else {
+                // Add the current node to the "greater than or equal to" list.
+                greaterTail.next = current;
+                greaterTail = greaterTail.next;
+            }
+            current = current.next;
+        }
+
+        // Terminate the "greater than" list to avoid cycles.
+        greaterTail.next = null;
+
+        // Connect the "less than" list with the "greater than" list.
+        lessTail.next = greaterHead.next;
+
+        // The head of the final list is the start of the "less than" list.
+        return lessHead.next;
+    }
     
     
 }
