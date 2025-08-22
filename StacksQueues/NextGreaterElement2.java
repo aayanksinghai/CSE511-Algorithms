cc87e7e0e41a5c415a257636360f809d115d5eab
aayanksinghai <aayanksinghai02@gmail.com>
2025-08-23 01:15:50 +0530

    Next Greater Element using Stack

--- StacksQueues/NextGreaterElement2.java
+++ StacksQueues/NextGreaterElement2.java
@@ -1,3 +1,18 @@
+/*
+ * Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), 
+ * return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in the array, 
+which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+ * 
+ * Link: https://leetcode.com/problems/next-greater-element-ii/description/?envType=problem-list-v2&envId=stack
+ * 
+ * Author: AAYANK SINGHAI (MT2025001)
+ */
+
+
+import java.util.Stack;
+
 class Solution {
     public int[] nextGreaterElements(int[] arr) {
         // Brute Force Approach 
@@ -28,7 +43,24 @@ class Solution {
         */
 
         // Optimal Approach Using Stack
-        
-        
+        // Optimal Approach Using Stack
+        Stack<Integer> st = new Stack<>();        
+        int len = arr.length;
+        int[] ans = new int[len];
+
+        for(int i = 2*len-1; i >= 0; i--){
+            while(!st.isEmpty() && st.peek() <= arr[i%len]){
+                st.pop();
+            }
+            if(i < len){
+                if(st.isEmpty()){
+                    ans[i] = -1;
+                }else{
+                    ans[i] = st.peek();
+                }
+            }
+            st.push(arr[i%len]);
+        }
+        return ans;    
     }
 }
