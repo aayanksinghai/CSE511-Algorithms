bca5416a4fd168e8e51cd8c0fc0a69475668d4bc
aayanksinghai <aayanksinghai02@gmail.com>
2025-08-23 22:51:14 +0530

    Maximal Rectangle

--- StacksQueues/MaximalRectangle.java
+++ StacksQueues/MaximalRectangle.java
@@ -6,10 +6,89 @@
  * Author: AAYANK SINGHAI (MT2025001)
  */
 
+import java.util.Stack;
 
 public class MaximalRectangle {
     public int maximalRectangle(char[][] matrix) {
-        
+        //Size of matrix
+       int n = matrix.length;
+       int m = matrix[0].length;
+
+       int[][] prefixSum = new int[n][m]; //storing different ground level heights
+
+       for(int j = 0; j < m; j++){
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += Character.getNumericValue(matrix[i][j]);
+
+            if(Character.getNumericValue(matrix[i][j]) == 0){
+                prefixSum[i][j] = 0;
+                sum = 0;
+            }else{
+                prefixSum[i][j] = sum;
+            }
+        }
+       }
+
+       int maxArea = 0;
+
+       for(int i = 0; i < n; i++){
+        int area  =  largestAreaHistogram(prefixSum[i]);
+        maxArea = Math.max(area, maxArea);
+       }
+
+       return maxArea;
     }
-    
+
+
+    public static int largestAreaHistogram(int[] heights){
+        int[] NSE = findNSE(heights);
+        int[] PSE = findPSE(heights);
+
+        int largestAreaOfRectangle = 0;
+
+        for(int i = 0; i < heights.length; i++){
+            largestAreaOfRectangle = Math.max(largestAreaOfRectangle, heights[i]*(NSE[i]-PSE[i]-1));
+        }
+        return largestAreaOfRectangle;
+    }
+
+    public static int[] findNSE(int[] arr){
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[arr.length];
+        
+        for(int i = arr.length-1; i >= 0; i--){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop(); //Keep popping until the top element is not smaller
+            }
+
+            if(!st.isEmpty()){
+                ans[i] = st.peek();
+            }else{
+                ans[i] = arr.length;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+
+    public static int[] findPSE(int[] arr){
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[arr.length];
+
+        for(int i = 0; i < arr.length; i++){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
+            }
+
+            if(!st.isEmpty()){
+                ans[i] = st.peek();
+            }else{
+                ans[i] = -1;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
 }
