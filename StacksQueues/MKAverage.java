8329acef48b5d155120e2d0a2f995f5bfefeaa1c
aayanksinghai <aayanksinghai02@gmail.com>
2025-08-24 23:05:58 +0530

    MK Average

--- StacksQueues/MKAverage.java
+++ StacksQueues/MKAverage.java
@@ -6,10 +6,83 @@
  * Author: AAYANK SINGHAI (MT2025001)
  */
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MKAverage {
 
-    
+    private final int m; //size of sliding window
+    private final int k; //k: no of smallest/largest elements excluding from average
+    private final Deque<Integer> window;
+    private final TreeMap<Integer, Integer> valueCnt;
+    private long windowSum;
+
+    public MKAverage(int m, int k) {
+        this.m = m;
+        this.k = k;
+        this.window = new ArrayDeque<>();
+        this.valueCnt = new TreeMap<>();
+        this.windowSum = 0;
+    }
+
+    public void addElement(int num) {
+        window.addLast(num);
+        valueCnt.put(num, valueCnt.getOrDefault(num, 0) + 1);
+        windowSum += num;
+
+        if (window.size() > m) {
+            int oldEle = window.removeFirst();
+            
+            
+            windowSum -= oldEle;
+            int count = valueCnt.get(oldEle);
+            if (count == 1) {
+                valueCnt.remove(oldEle); 
+            } else {
+                valueCnt.put(oldEle, count - 1);
+            }
+        }
+    }
+
+    public int calculateMKAverage() {
+        if (window.size() < m) {
+            return -1;
+        }
+
+        long sumAvg = windowSum;
+        int elementsTrim = k;
+
+        for (Map.Entry<Integer, Integer> entry : valueCnt.entrySet()) {
+            int value = entry.getKey();
+            int freq = entry.getValue();
+
+            int trimCount = Math.min(elementsTrim, freq);
+            sumAvg -= (long) value * trimCount;
+            elementsTrim -= trimCount;
+
+            if (elementsTrim == 0) {
+                break;
+            }
+        }
+
+        elementsTrim = k;
+        for (Map.Entry<Integer, Integer> entry : valueCnt.descendingMap().entrySet()) {
+            int value = entry.getKey();
+            int freq = entry.getValue();
+
+            int trimCount = Math.min(elementsTrim, freq);
+            sumAvg -= (long) value * trimCount;
+            elementsTrim -= trimCount;
+
+            if (elementsTrim == 0) {
+                break;
+            }
+        }
+        
+        return (int) (sumAvg / (m - 2 * k));
+    }
 }
 
 
